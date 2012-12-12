efec39451305e8560448cb3616a65d0624be36d2
isidro.garcia <isidro.garcia@gmail.com>
2012-12-12 20:36:08 +0000

    Updating account with shared preferences

---
+++ src/org/pezke/misdatos/util/CommonConstants.java
@@ -0,0 +1,10 @@
+package org.pezke.misdatos.util;
+
+public class CommonConstants {
+
+	public static final String DATA 	= "org.pezke.misdatos";
+	public static final String LOGIN 	= "a$";
+	
+	public static final String PASSWORD = "preferencePassword";
+	
+}
