d5a05cdf341330306c2b421b2b2c4cfa7ecd8785
isidro.garcia <isidro.garcia@gmail.com>
2014-02-25 20:33:47 +0000

    Prevent main activity from being in background when it is paused.

--- src/org/pezke/misdatos/util/CommonConstants.java
+++ src/org/pezke/misdatos/util/CommonConstants.java
@@ -9,7 +9,8 @@ public class CommonConstants {
 	
 	public static final String DELETE_ACCOUNT = "preferenceDeleteAccount";
 	
-	public static final int REQUEST_CONFIG_ACTION 	= 200;
-	public static final int RESULT_DELETE_ACCOUNT   = 201;
+	public static final int REQUEST_CONFIG_ACTION 		= 200;
+	public static final int RESULT_DELETE_ACCOUNT   	= 201;
+	public static final int RESULT_PAUSED_CONFIGURATION = 202;
 	
 }
